6bb56c89696b7cb43da49a2c74646484907f1f39
alexmartinezm <alexmdevs@gmail.com>
2018-05-22 12:28:33 +0200

    Added controller.

---
+++ TodoApi/Controllers/ToDoItemsController.cs
@@ -0,0 +1,19 @@
+﻿using ToDoApi.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoItemsController : Controller
+    {
+        private readonly IToDoRepository _toDoRepository;
+
+        public ToDoItemsController(IToDoRepository toDoRepository)
+        {
+            _toDoRepository = toDoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult List() => Ok(_toDoRepository.All);
+    }
+}
