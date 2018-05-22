5f9b9315ba5a9c6bc892369083f6a25d74102d06
alexmartinezm <alexmdevs@gmail.com>
2018-05-22 12:10:01 +0200

    Added model.

---
+++ TodoApi/Models/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ToDoApi.Models
+{
+	public class ToDoItem
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string ImageName { get; set; }
+	}
+}
